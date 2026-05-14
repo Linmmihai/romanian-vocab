8626c758f903b3f0af8678a0f69eff95325be0bc
Linmmihai <linmihai2004@gmail.com>
2026-05-14 18:08:20 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,18 @@
-const CACHE_NAME = 'ro-vocab-pwa-v4';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './api.js',
-  './auth.js',
-  './app.js?v=20260508-1',
-  './stress_grammar_patch.js',
-  './manifest.webmanifest',
-  './apple-touch-icon.png',
-  './icons/apple-touch-icon.png',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
+const CACHE_NAME = 'ro-vocab-pwa-v6';
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
   if (event.request.method !== 'GET') return;
-  if (url.origin !== self.location.origin) return;
-
-  event.respondWith(
-    fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
